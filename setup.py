fee669cca695f3072d6e0cb82fd9d4313dde48d6
Chou-Uken <chouuken@outlook.com>
2023-12-25 10:19:01 +0800

    final1

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='biofkit',
-    version='0.0.1',
-    description=('With biofkit, sequence can be easily extract. And more functions will be added.'),
+    version='0.0.2',
+    conda_buildnum=1,
+    conda_buildstr=py39_0,
+    description=('With biofkit, sequence can be easily extract from pdb. And more functions will be added.'),
     long_description=open('README.md').read(),
     author='Zhang Yujian',
     author_email='Chouuken@outlook.com',
